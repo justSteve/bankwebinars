8c86f09eb69a4fcbd5dde40344dbda10532893ba
justSteve <steve@juststeve.com>
2013-12-18 19:26:51 -0600

    Runs here.

---
+++ CUWebinars.Business/Models/TimeZone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CUWebinars.Business.Models
+{
+
+    public enum USTimeZone
+    {
+        Pacific = 1,
+        Mountain = 2,
+        Central = 3,
+        Eastern = 4
+    }
+}
---
+++ CUWebinars.Business/Models/WebinarStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CUWebinars.Business.Models
+{
+    public enum WebinarStatus
+    {
+        OnHold = 1,
+        Scheduled = 2,
+        Recorded = 3,
+        Archived = 4,
+        Deleted = 5
+    }
+}
