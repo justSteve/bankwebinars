5a1efd03cb2acec7d957db36c7c1261cb3f7ff69
David Rogers <davidrogersau@yahoo.com.au>
2014-02-06 12:46:12 +1030

    Fixed signature of FindOptionsByWebinarId in IRefDataRepository.

--- CUWebinars.Business/Repository/IRefDataRepository.cs
+++ CUWebinars.Business/Repository/IRefDataRepository.cs
@@ -5,7 +5,7 @@ namespace CUWebinars.Business.Repository
 {
     public interface IRefDataRepository
     {
-        IQueryable<Option> FindOptionsByWebinarId(int id);
+        IList<Option> FindOptionsByWebinarId(int id);
         IEnumerable<Address> GetAddressesForUser(int id);
         IQueryable<Institution> GetInstitutions();
         WebUser GetWebUserByEmail(string email);        
