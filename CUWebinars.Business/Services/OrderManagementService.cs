144e9344ba7a77ebb95060c85d55360f09d6ad0e
David Rogers <davidrogersau@yahoo.com.au>
2014-02-06 15:29:21 +1030

    OrderManagementService now implements IOrderManagementService.

--- CUWebinars.Business/Services/OrderManagementService.cs
+++ CUWebinars.Business/Services/OrderManagementService.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using CUWebinars.Business.Models;
 using CUWebinars.Business.Repository;
 
 namespace CUWebinars.Business.Services
 {
-    public class OrderManagementService
+    public class OrderManagementService : IOrderManagementService
     {
         private readonly IOptionRepository _optionRepository;
         private IRefDataRepository _refDataRepository;
