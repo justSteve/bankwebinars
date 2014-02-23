e7b9ae3e48fcce0d8c04e1a73dd54d657237b716
David Rogers <davidrogersau@yahoo.com.au>
2014-02-24 08:16:23 +1030

    Added two files which were untracked.

---
+++ CUWebinars.Web/Models/ChangeEmailFromKeyInputModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace CUWebinars.Web.Models
+{
+    
+    public class ChangeEmailFromKeyInputModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [HiddenInput]
+        public string Key { get; set; }
+    }
+}
