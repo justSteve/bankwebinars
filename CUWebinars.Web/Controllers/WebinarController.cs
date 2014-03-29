a57f0048c9dd9fcadf1e4fe08b81142738f6f8d2
justSteve <steve@juststeve.com>
2014-03-29 11:06:01 -0500

    Adds GTW CreateRegistrant api

--- CUWebinars.Web/Controllers/WebinarController.cs
+++ CUWebinars.Web/Controllers/WebinarController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Security.Cryptography;
+using System.Text;
 using System.Threading;
 using System.Web.Http;
 using System.Web.Mvc;
