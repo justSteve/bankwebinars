53570338f9f0bbc63ee124b275fca8f99cdd10f5
David Rogers <davidrogersau@yahoo.com.au>
2014-04-24 16:02:35 +0930

    Added logic to CSV Parser to parse csv files with commas within fields.

--- CUWebinars.Web/Core/CsvParseOps.cs
+++ CUWebinars.Web/Core/CsvParseOps.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Microsoft.VisualBasic.FileIO;
 
 namespace CUWebinars.Web.Core
 {
@@ -16,65 +17,57 @@ namespace CUWebinars.Web.Core
         {
             IList<IncomingOrderModel> incomingOrderModels = new List<IncomingOrderModel>();
 
-            string line = string.Empty;
-
-            using (var sr = new StreamReader(stream))
+            using (var textFieldParser = new TextFieldParser(stream))
             {
-                string[] keys, values;
-                keys = values = null;
+                textFieldParser.TextFieldType = FieldType.Delimited;
+                textFieldParser.SetDelimiters(",");
+                textFieldParser.CommentTokens = new[] { "PaymentStatus" }; //  Discard header - PaymentStatus is first cell at A,1
 
-                line = sr.ReadLine();
-
-                if (line != null)
-                    keys = line.Split(",".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-                else
-                    throw new NullReferenceException("The header section of the csv file is missing.");
+                var lines = new List<string[]>();
 
 
-                while((line = sr.ReadLine()) != null)
+                while(!textFieldParser.EndOfData)
+                {
+                    var lineAsSeparatedFields = textFieldParser.ReadFields();
+                    lines.Add(lineAsSeparatedFields);
+                }
+
+                lines.ForEach((fields) =>
                 {
                     var incomingOrderModel = new IncomingOrderModel
                     {
                         BillingAddress = new Address(),
                         ShippingAddress = new Address()
                     };
-                    var dataDictionary = new Dictionary<string, string>(keys.Length);
 
-                    values = line.Split(',');
-
-                    for (int i = 0; i < keys.Length; i++)
-                    {
-                        dataDictionary.Add(keys[i].Trim(), values[i].Trim());
-                    }
-
-                    incomingOrderModel.Status = (OrderStatus)Enum.Parse(typeof(OrderStatus), dataDictionary[keys[0]], true);
+                    incomingOrderModel.Status = (OrderStatus)Enum.Parse(typeof(OrderStatus), fields[0], true);
 
                     //  Additional Locations are at index 1
-                    var addresses = dataDictionary[keys[1]].Split(";".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                    var addresses = fields[1].Split(";".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
                     var additionalLocations = new List<IncomingAdditionalLocation>(addresses.Length);
-                    additionalLocations.AddRange(addresses.Select(address => new IncomingAdditionalLocation {Email = address}));
+                    additionalLocations.AddRange(addresses.Select(address => new IncomingAdditionalLocation { Email = address }));
                     incomingOrderModel.AdditionalLocation = additionalLocations;
 
-                    incomingOrderModel.idWebinar = int.Parse(dataDictionary[keys[2]]);
-                    incomingOrderModel.idRegType = int.Parse(dataDictionary[keys[3]]);
-                    incomingOrderModel.idAffiliate = int.Parse(dataDictionary[keys[4]]);
-                    incomingOrderModel.FirstName = dataDictionary[keys[5]];
-                    incomingOrderModel.LastName = dataDictionary[keys[6]];
-                    incomingOrderModel.Institution = dataDictionary[keys[7]];
-                    incomingOrderModel.Title = dataDictionary[keys[8]]; ;
-                    incomingOrderModel.Email = dataDictionary[keys[9]];
+                    incomingOrderModel.idWebinar = int.Parse(fields[2]);
+                    incomingOrderModel.idRegType = int.Parse(fields[3]);
+                    incomingOrderModel.idAffiliate = int.Parse(fields[4]);
+                    incomingOrderModel.FirstName = fields[5];
+                    incomingOrderModel.LastName = fields[6];
+                    incomingOrderModel.Institution = fields[7];
+                    incomingOrderModel.Title = fields[8]; ;
+                    incomingOrderModel.Email = fields[9];
                     incomingOrderModel.BillingAddress.Name = string.Concat(incomingOrderModel.FirstName, " ", incomingOrderModel.LastName);
-                    incomingOrderModel.BillingAddress.Phone = dataDictionary[keys[10]];
-                    incomingOrderModel.BillingAddress.StreetAddress = dataDictionary[keys[11]];
-                    incomingOrderModel.BillingAddress.StreetAddress2 = dataDictionary[keys[12]];
-                    incomingOrderModel.BillingAddress.City = dataDictionary[keys[13]];
-                    incomingOrderModel.BillingAddress.State = dataDictionary[keys[14]];
-                    incomingOrderModel.BillingAddress.Zip = dataDictionary[keys[15]];
-                    incomingOrderModel.BillingAddress.Country = dataDictionary[keys[16]];
+                    incomingOrderModel.BillingAddress.Phone = fields[10];
+                    incomingOrderModel.BillingAddress.StreetAddress = fields[11];
+                    incomingOrderModel.BillingAddress.StreetAddress2 = fields[12];
+                    incomingOrderModel.BillingAddress.City = fields[13];
+                    incomingOrderModel.BillingAddress.State = fields[14];
+                    incomingOrderModel.BillingAddress.Zip = fields[15];
+                    incomingOrderModel.BillingAddress.Country = fields[16];
                     incomingOrderModel.BillingAddress.AddressType = WebUiConstants.BillingAddress;
 
                     //  If there is a name for the shipping address, we need to parse the address
-                    var name = dataDictionary[keys[17]];
+                    var name = fields[17];
 
                     if (string.IsNullOrWhiteSpace(name))
                     {
@@ -83,16 +76,16 @@ namespace CUWebinars.Web.Core
                     else
                     {
                         incomingOrderModel.ShippingAddress.Name = name;
-                        incomingOrderModel.ShippingAddress.Phone = dataDictionary[keys[18]];
-                        incomingOrderModel.ShippingAddress.StreetAddress = dataDictionary[keys[19]];
-                        incomingOrderModel.ShippingAddress.StreetAddress2 = dataDictionary[keys[20]];
-                        incomingOrderModel.ShippingAddress.City = dataDictionary[keys[21]];
-                        incomingOrderModel.ShippingAddress.State = dataDictionary[keys[22]];
-                        incomingOrderModel.ShippingAddress.Zip = dataDictionary[keys[23]];
-                        incomingOrderModel.ShippingAddress.Country = dataDictionary[keys[24]];
+                        incomingOrderModel.ShippingAddress.Phone = fields[18];
+                        incomingOrderModel.ShippingAddress.StreetAddress = fields[19];
+                        incomingOrderModel.ShippingAddress.StreetAddress2 = fields[20];
+                        incomingOrderModel.ShippingAddress.City = fields[21];
+                        incomingOrderModel.ShippingAddress.State = fields[22];
+                        incomingOrderModel.ShippingAddress.Zip = fields[23];
+                        incomingOrderModel.ShippingAddress.Country = fields[24];
                         incomingOrderModel.ShippingAddress.AddressType = WebUiConstants.ShippingAddress;
-                    }
-                }
+                    }                    
+                });
 
                 return incomingOrderModels;
             }
