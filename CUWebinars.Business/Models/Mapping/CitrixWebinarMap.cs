ef50121680227078d23f01c85d2d44016244f12c
David Rogers <davidrogersau@yahoo.com.au>
2014-05-06 20:54:54 +0930

    Missed a change.

--- CUWebinars.Business/Models/Mapping/CitrixWebinarMap.cs
+++ CUWebinars.Business/Models/Mapping/CitrixWebinarMap.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity.ModelConfiguration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.Entity.ModelConfiguration;
 
 namespace CUWebinars.Business.Models.Mapping
 {
@@ -13,7 +8,7 @@ namespace CUWebinars.Business.Models.Mapping
         {
             //  Need to make decisions above field sizes etc.
 
-
+            ToTable("CitrixWebinar");
 
         }
     }
