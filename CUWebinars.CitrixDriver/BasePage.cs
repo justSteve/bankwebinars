52d6b7d4472f8d0f44c781b9bfa52bb1c80ae32d
David Rogers <davidrogersau@yahoo.com.au>
2014-05-07 17:42:37 +0930

    Kicked off the code to web-drive the Citrix create a webinar flow.

---
+++ CUWebinars.CitrixDriver/BasePage.cs
@@ -0,0 +1,33 @@
+﻿using CUWebinars.Selenium.Core;
+
+namespace CUWebinars.CitrixDriver
+{
+    public class BasePage
+    {
+        protected string Url { get; set; }
+
+        protected ITestDriver SeleniumTestDriver { get; set; }
+
+        public virtual void Open()
+        {
+            if (null != SeleniumTestDriver)
+            {
+                SeleniumTestDriver.GoToUrl(Url);
+            }
+        }
+
+        public virtual string Title
+        {
+            get
+            {
+                return SeleniumTestDriver.GetDocumentTitle();
+            }
+        }
+
+        public virtual void Close()
+        {
+            SeleniumTestDriver.Quit();
+        }
+
+    }
+}
---
+++ CUWebinars.CitrixDriver/CitrixWebPage.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using CUWebinars.Selenium.Core;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace CUWebinars.CitrixDriver
+{
+    public class CitrixWebPage : BasePage
+    {
+        public CitrixWebPage(ITestDriver seleniumTestDriver, string url)
+        {
+            Url = url;
+            SeleniumTestDriver = seleniumTestDriver;
+        }
+
+        public void Wait(int milliSeconds = 1000)
+        {
+            SeleniumTestDriver.Wait(milliSeconds);
+        }
+
+        public void OpenPage(string url)
+        {
+            SeleniumTestDriver.GoToUrl(url);
+        }
+
+        public bool HeadingIsPresentOnPage
+        {
+            get
+            {
+                return SeleniumTestDriver
+                    .FindByCssSelector("h1")
+                    .Text
+                    .Equals("Test", StringComparison.Ordinal);
+            }
+        }
+
+        public void ClearCookies()
+        {
+            SeleniumTestDriver.ClearCookies();
+        }
+
+        public void ClickLoginLink()
+        {
+            SeleniumTestDriver.FindByIdClick("btnLogin");
+        }
+
+        public void GotToLoginPage()
+        {
+            SeleniumTestDriver.FindByLinkTextClick("Log In");
+        }
+
+
+        public void EnterEmailAddress(string email)
+        {
+            SeleniumTestDriver.Wait(500);
+            IWebElement emailInput = SeleniumTestDriver.FindById("emailAddress");
+            emailInput.Clear();
+            emailInput.SendKeys(email);
+        }
+
+        public void EnterPasswordWhereUserExists(string password)
+        {
+            SeleniumTestDriver.Wait(500);
+            IWebElement passwordInput = SeleniumTestDriver.FindById("password");
+            passwordInput.Clear();
+            passwordInput.SendKeys(password);
+        }
+
+        public void LogOff()
+        {
+            //SeleniumTestDriver.FindByXPathClick(Constants.LogoffLinkPath);
+        }
+
+        public void ClickSubmit()
+        {
+            SeleniumTestDriver.FindByIdClick("submit");
+        }
+
+        public void GoToWebinarsPage()
+        {
+            SeleniumTestDriver.FindByLinkTextClick("My Webinars");
+        }
+
+        public void ScheduleAWebinar()
+        {
+            SeleniumTestDriver.FindByLinkTextClick("Schedule a Webinar");
+        }
+
+        public void ChooseWebinarTemplate()
+        {
+            SeleniumTestDriver.FindByIdClick("WebinarChoice");
+            SeleniumTestDriver.Wait(500);
+            SeleniumTestDriver.FindByCssSelectorClick("option[value=\"563748522|wid\"]");
+        }
+
+        public void CompleteDetails()
+        {
+            SeleniumTestDriver.FindByIdClick("privateConfCallRadio");
+            SeleniumTestDriver.FindByIdClick("StartDate_Cal_0");
+            SeleniumTestDriver.FindByCssSelectorClick("img.next");
+
+            var dateWidget = SeleniumTestDriver.FindByCssSelectorClick("div#floatcal table");
+            ReadOnlyCollection<IWebElement> rows = dateWidget.FindElements(By.TagName("tr"));
+            ReadOnlyCollection<IWebElement> cols = dateWidget.FindElements(By.TagName("td"));
+
+            //  Lets choose the 28th of the current month
+            foreach (IWebElement webElement in cols)
+            {
+                if (webElement.Text.Equals("28"))
+                {
+                    webElement.Click();
+                    break;
+                }
+            }
+            
+            SeleniumTestDriver.TypeText("StartHour_0", "06:00");
+            
+            var startMeridian = new SelectElement(SeleniumTestDriver.FindById("StartAMPM_0"));
+            startMeridian.SelectByText("PM");
+            SeleniumTestDriver.TypeText("EndHour_0", "11:00");
+
+            var endMeridian = new SelectElement(SeleniumTestDriver.FindById("EndAMPM_0"));
+            endMeridian.SelectByText("PM");
+
+            var timeZoneKey = new SelectElement(SeleniumTestDriver.FindById("TimeZoneKey"));
+            timeZoneKey.SelectByValue("64");
+        }
+    }
+}
