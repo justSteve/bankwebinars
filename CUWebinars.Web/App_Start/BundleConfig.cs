a722e45d3b037b8be14176ccd0cb7f604d7f5838
David Rogers <davidrogersau@yahoo.com.au>
2014-09-08 22:54:34 +0930

    Raft of small changes.

--- CUWebinars.Web/App_Start/BundleConfig.cs
+++ CUWebinars.Web/App_Start/BundleConfig.cs
@@ -77,10 +77,10 @@ namespace CUWebinars.Web.App_Start
                         "~/Scripts/app/web-user-details-register.js"
             ));
 
-            //bundles.Add(new ScriptBundle("~/bundles/membership-notifications-ops").Include(
-            //    "~/Scripts/app/membership-notifications.js",
-            //    "~/Scripts/app/web-user-details-register.js"
-            //));
+            bundles.Add(new ScriptBundle("~/bundles/membership-notifications-ops").Include(
+                "~/Scripts/app/membership-notifications.js",
+                "~/Scripts/app/web-user-details-register.js"
+            ));
 
             bundles.Add(new StyleBundle("~/Content/CrispCSS").Include(
                     "~/Content/css/styleCrisp.css",
