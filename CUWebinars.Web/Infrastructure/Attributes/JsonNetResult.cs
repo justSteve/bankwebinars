1aba72110324a724f526b3f520e40708fa0ddc4b
David Rogers <davidrogersau@yahoo.com.au>
2014-09-21 18:33:13 +0930

    Added extension methods which will be a more elegant solution for returning ModelState errors to the client.

---
+++ CUWebinars.Web/Infrastructure/Attributes/JsonNetResult.cs
@@ -0,0 +1,49 @@
+﻿using CUWebinars.Web.Infrastructure.Extensions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Text;
+using System.Web.Mvc;
+
+namespace CUWebinars.Web.Infrastructure.Attributes
+{
+    public class JsonNetResult : ActionResult
+    {
+        public Encoding ContentEncoding { get; set; }
+        public string ContentType { get; set; }
+        public object Data { get; set; }
+
+        public IsoDateTimeConverter IsoDateTimeConverter { get; set; }
+        public JsonSerializerSettings SerializerSettings { get; set; }
+        public Formatting Formatting { get; set; }
+
+        public JsonNetResult()
+        {
+            SerializerSettings = new JsonSerializerSettings();
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null) throw new ArgumentNullException("context");
+
+            var response = context.HttpContext.Response;
+
+            response.ContentType = !string.IsNullOrEmpty(ContentType)
+              ? ContentType
+              : "application/json";
+
+            response.CacheControl = "no-cache";
+
+            if (ContentEncoding != null) response.ContentEncoding = ContentEncoding;
+
+            // Here we call the extension method Object.ToJsonNet().
+            if (Data != null)
+            {
+                if (IsoDateTimeConverter != null && Formatting != null)
+                    response.Write(Data.ToJsonNet(SerializerSettings, Formatting, IsoDateTimeConverter));
+                else
+                    response.Write(Data.ToJsonNet());
+            }
+        }
+    }
+}
---
+++ CUWebinars.Web/Infrastructure/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CUWebinars.Web.Infrastructure.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string JoinAsString(this IEnumerable<string> src, string separator = ",")
+        {
+            if (ReferenceEquals(src, null))
+                return string.Empty;
+
+            var enumerable = src as string[] ?? src.ToArray();
+
+            return enumerable.Any() ? string.Join(separator, enumerable) : string.Empty;
+        }
+    }
+}
