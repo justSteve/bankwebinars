bddd180de280531032fb620ce4ca3b35d2b3aa7e
David Rogers <davidrogersau@yahoo.com.au>
2014-11-19 08:36:04 +1030

    Added RegisterFieldsDTO.

---
+++ CUWebinars.Web/Models/RegisterFieldsDTO.cs
@@ -0,0 +1,10 @@
+﻿
+namespace CUWebinars.Web.Models
+{
+    public class RegisterFieldsDTO
+    {
+        public string Email { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+    }
+}
