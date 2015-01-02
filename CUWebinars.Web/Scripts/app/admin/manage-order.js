7aa05631b7e3779bf7e0d764831f843d3336437e
David Rogers <davidrogersau@yahoo.com.au>
2015-01-02 23:36:01 +1030

    Further work on the Manage Order screen. Mostly done now. Need to do back-end stuff yet.

--- CUWebinars.Web/Scripts/app/admin/manage-order.js
+++ CUWebinars.Web/Scripts/app/admin/manage-order.js
@@ -9,6 +9,9 @@ $(function () {
     $('#getOrderButton').on('click', function () {
         MANAGE.idOrder = orderIdInput.val();
 
+        // loading spinner
+        orderIdInput.append('<span id="spinWrapper" class="label label-info"><i id="spinner" class="icon-spinner icon-spin"></i>&nbsp;loading...</span>');
+
         $('#orderRelatedFields').load('/Admin/GetOrderDetails/' + MANAGE.idOrder, function () {
 
             MANAGE.locationsSpanPrefix = 'LocationSpan-';
@@ -16,12 +19,14 @@ $(function () {
             
             MANAGE.primeDomVariables();
             MANAGE.wireUpHandlers();
-            MANAGE.numberAddLocsLabel = $('#nrAddLocs');
+            
             MANAGE.addLocsUnitPrice = $('#CostPerAdditionalLocation').val();
             MANAGE.addLocsTotalPrice = $('#DisplayRowPriceViewModel_PricesAndDiscounts_TotalOptions').val();
             MANAGE.retrivedAdditionalLocations = false;
 
             MANAGE.wireUpTrashIcons();
+
+            $('#spinWrapper').remove();
         });
     });
 });
@@ -34,6 +39,9 @@ MANAGE.addAdditionalLocation = function (e) {
 
     if (MANAGE.numberOfAdditionalLocations === 0) {
         newId = 0;
+        var naSpan =$('#naText');
+        if (naSpan.length > 0)
+            naSpan.remove();
     } else {
         // first get the last previous email input
         var lastInput = MANAGE.wrapperDiv.find('input[type="email"]:last');
@@ -52,6 +60,7 @@ MANAGE.addAdditionalLocation = function (e) {
     $('#' + additionalLocationEmailId).focus();
 
     MANAGE.numberOfAdditionalLocations += 1;
+    MANAGE.adjustAdditionalLocationsTotal(MANAGE.numberOfAdditionalLocations);
 };
 
 MANAGE.deleteItem = function (e) {
@@ -71,6 +80,8 @@ MANAGE.deleteItem = function (e) {
     $('#' + idx + MANAGE.breakSuffix).hide(500, function () {
         $(this).remove();
     });
+
+    MANAGE.adjustAdditionalLocationsTotal(MANAGE.numberOfAdditionalLocations);
 };
 
 MANAGE.wireUpTrashIcons = function () {
@@ -85,154 +96,94 @@ MANAGE.wireUpTrashIcons = function () {
 
 MANAGE.primeDomVariables = function() {
     MANAGE.wrapperDiv = $('#collectAdditionalLocations');
+    MANAGE.numberAddLocsLabel = $('#nrAddLocs');
+    MANAGE.addAdditionalLocationsButton = $('#addLocationsButton');
+    MANAGE.totalOptionsInput = $('DisplayRowPriceViewModel_PricesAndDiscounts_TotalOptions');
+
+};
+
+MANAGE.submitForm = function(e) {
+    e.preventDefault();
+
+    var emailInputs = MANAGE.wrapperDiv.find('input[type="email"]');
+
+    $.each(emailInputs, function (idx, i) {
+        $(i).attr('name', 'AdditionalLocations[' + idx + '].Email');
+    });
+
+    var form = $('#manageOrderForm');
+
+    $.ajax({
+        type: 'POST',
+        contentType: constants.FormPostContentType,
+        cache: false,
+        url: form.attr('action'),
+        dataType: constants.JsonDataType,
+        data: form.serialize(),
+        beforeSend: function () {
+            // this is where we append a loading image
+            //$('#labelEmail').html('<span class="label label-warning">&nbsp;&nbsp;<i class="icon-spinner icon-spin "></i>&nbsp;&nbsp;Checking that Email...</span>');
+        }
+    }).done(function (data, bla, bla) {
+
+    });
+
+
+};
+
+MANAGE.changeRegType = function(e) {
+
+    e.preventDefault();
+
+    var optionId = $(this).val();
+
+    var self = this;
+
+    $.ajax({
+        url: "/cart/CheckIfAddLocShouldHide?optionID=" + optionId,
+        type: "GET",
+        cache: false,
+        dataType: constants.JsonDataType,
+
+        beforeSend: function () {
+            //console.log('beforeSend CheckIfAddLocShouldHide');
+            // no loading image needed
+        }
+    }).done(function (data) {
+        //console.log('done CheckIfAddLocShouldHide');
+        if (data.shouldShow === 'Yes') {
+            MANAGE.addAdditionalLocationsButton.removeAttr('disabled');
+        } else if (data.shouldShow === 'No') {
+            //console.log('hide  CheckIfAddLocShouldHide');
+            $('#collectAdditionalLocations').empty().html('<span id="naText" class="text text-info">Not applicable for this RegType</span>');
+            MANAGE.numberOfAdditionalLocations = 0;
+            MANAGE.numberAddLocsLabel.text(0);
+            MANAGE.addAdditionalLocationsButton.attr('disabled', 'disabled');
+        }
+
+        //if (data.shippingDetailsRqrd === 'Yes') {
+        //    self.shippingAddressRequired = true;
+        //} else {
+        //    self.shippingAddressRequired = false;
+        //}
+    }).fail(function (data) {
+        //console.log('CheckIfAddLocShouldHide failed!!! ');
+    });
 };
 
 MANAGE.wireUpHandlers = function () {
 
     $('#addLocationsButton').on('click', MANAGE.addAdditionalLocation);
-    $('#editOrderSubmitButton').on('click', function (e) {
-
-        e.preventDefault();
-
-        var emailInputs = MANAGE.wrapperDiv.find('input[type="email"]');
-
-        $.each(emailInputs, function (idx, i) {
-            $(i).attr('name', 'AdditionalLocations[' + idx + '].Email');
-        });
-
-        var form = $('#manageOrderForm');
-
-        $.ajax({
-            type: 'POST',
-            contentType: constants.FormPostContentType,
-            cache: false,
-            url: form.attr('action'),
-            dataType: constants.JsonDataType,
-            data: form.serialize(),
-            beforeSend: function() {
-                // this is where we append a loading image
-                //$('#labelEmail').html('<span class="label label-warning">&nbsp;&nbsp;<i class="icon-spinner icon-spin "></i>&nbsp;&nbsp;Checking that Email...</span>');
-            }
-        }).done(function(data, bla, bla) {
-
-        }).fail(function(data, bla, bla) {
-            
-        }).always(function(data, bla, bla) {
-            
-        });
-
-    });
-
-
+    $('#editOrderSubmitButton').on('click', MANAGE.submitForm);
+    $('#RegType').on('change', MANAGE.changeRegType);
 };
 
-MANAGE.wireUpHandlersForModal = function() {
-    var locationsCloned, locationsBakForCancel;
-    var collectAdditionalLocations = $('#collectAdditionalLocations');
-    var locations = collectAdditionalLocations.children();
 
-    if (locations.length > 0) {
-        locationsCloned = locations.clone();
-        locationsBakForCancel = locations.clone();
-    }
+MANAGE.adjustAdditionalLocationsTotal = function(number) {
 
-    collectAdditionalLocations.empty();
+    MANAGE.numberAddLocsLabel.text(MANAGE.numberOfAdditionalLocations);
 
-    var emailInputsAdded = $.Deferred(function() {
-        additionalLocationEmailWrapper = $('#AdditionalLocationEmailWrapper');
-        if (locationsCloned)
-            additionalLocationEmailWrapper.append(locationsCloned);
-    });
+    var newPrice = number * parseFloat(MANAGE.addLocsUnitPrice);
 
-    $.when(emailInputsAdded.resolve()).then(function() {
-        numberOfAdditionalLocations = $('#AdditionalLocationEmailWrapper input[type="email"]').length;
-        MANAGE.numberAddLocsLabel.text(numberOfAdditionalLocations);
-
-        if (numberOfAdditionalLocations < 1) {
-            //$('#sumbitAdditionalLocationsButton').off('click');
-
-        } else {
-
-            if ($('#sumbitAdditionalLocationsButton').length < 1) {
-
-                $('#AdditionalLocationEmailWrapper').after($('<button>',
-                {
-                    id: 'sumbitAdditionalLocationsButton',
-                    text: 'Submit',
-                    'class': 'btn btn-primary',
-                }));
-
-                $('#sumbitAdditionalLocationsButton').on('click', function() {
-
-                    collectAdditionalLocations.empty();
-                    collectAdditionalLocations.append(additionalLocationEmailWrapper.children());
-
-                    $('#additionalLocationsModalDialog').modal('hide');
-
-                    $(this).remove();
-                });
-                var trashCans = additionalLocationEmailWrapper.find('i');
-
-                $.each(trashCans, function(idx, i) {
-                    $(i).on('click', MANAGE.deleteItem);
-                });
-            }
-        }
-
-        $('#AddInputsButton').on('click', function() {
-
-            if (numberOfAdditionalLocations === 0) {
-                $('#AdditionalLocationEmailWrapper').after($('<button>',
-                {
-                    id: 'sumbitAdditionalLocationsButton',
-                    text: 'Submit',
-                    'class': 'btn btn-primary',
-                }));
-
-                $('#sumbitAdditionalLocationsButton').on('click', function() {
-
-                    collectAdditionalLocations.empty();
-                    collectAdditionalLocations.append(additionalLocationEmailWrapper.children());
-
-                    $('#additionalLocationsModalDialog').modal('hide');
-
-                    $(this).remove();
-                });
-            }
-
-            var newId;
-            if (numberOfAdditionalLocations === 0) {
-                newId = 0;
-            } else {
-                // first get the last previous email input
-                var lastInput = additionalLocationEmailWrapper.find('input[type="email"]:last');
-                // get its id
-                var lastInputId = lastInput.attr('id');
-                var id = parseInt(lastInputId.charAt(lastInputId.length - 1));
-                newId = id + 1;
-            }
-            additionalLocationEmailWrapper.append('<span id="' + locationsSpanPrefix + newId + '"><input id="AdditionalLocationEmail_' + newId + '" name="AdditionalLocations[' + newId + '].Email" type="email" placeholder="Enter email address" />&nbsp;<i class="icon-trash icon-white" style="cursor: pointer" id="' + newId + '-AdditionLocationEmail-delete"></i></span> <br id="' + newId + breakSuffix + '">');
-            additionalLocationEmailWrapper.find('i#' + newId + '-AdditionLocationEmail-delete').on('click', MANAGE.deleteItem);
-            $('#AdditionalLocationEmail_' + newId).focus();
-            numberOfAdditionalLocations++;
-            MANAGE.adjustAdditionalLocationsData(numberOfAdditionalLocations);
-        });
-
-        $('#closeButton, #additionalLocationsModalDialog > div > div.modal-header > button').on('click', function(e) {
-            if (locationsBakForCancel) {
-
-                $.each(locationsBakForCancel.find('i'), function(idx, i) {
-                    $(i).on('click', MANAGE.deleteItem);
-                });
-                collectAdditionalLocations.append(locationsBakForCancel);
-            }
-
-        });
-    });
-};
-
-MANAGE.adjustAdditionalLocationsData = function(nr) {
-    var newAddLocsPrice = nr * MANAGE.addLocsUnitPrice;
-    MANAGE.numberAddLocsLabel.text(nr);
-    $('#DisplayRowPriceViewModel_PricesAndDiscounts_TotalOptions').val(newAddLocsPrice);
+    MANAGE.totalOptionsInput.val(newPrice);
 };
