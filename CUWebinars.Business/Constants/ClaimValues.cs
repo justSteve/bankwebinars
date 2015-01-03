2c434af23f80723339887414b24fcc7cad3b43ae
Stephen Hueners <steve@juststeve.com>
2015-01-03 02:09:13 -0600

    Stubs in views for Affiliate Checkout

--- CUWebinars.Business/Constants/ClaimValues.cs
+++ CUWebinars.Business/Constants/ClaimValues.cs
@@ -5,6 +5,7 @@ namespace CUWebinars.Business.Constants
     {
         public static readonly string CartRegistration = "CartRegistration";
         public static readonly string Admin = "Admin";
+        public static readonly string Affiliate = "Affiliate";
         public static readonly string Admin_CFTNOW = "Admin_CFTNOW";
         public static readonly string Admin_CFTWS = "Admin_CFTWS";
         public static readonly string ManualRegistration = "ManualRegistration";
