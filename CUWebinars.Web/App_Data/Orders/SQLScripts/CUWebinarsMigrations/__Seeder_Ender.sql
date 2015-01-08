6fdf0749c3db76abc817eb257996a80baa28467a
Stephen Hueners <steve@juststeve.com>
2015-01-08 17:38:47 -0600

    another migration gyration

--- CUWebinars.Web/App_Data/Orders/SQLScripts/CUWebinarsMigrations/__Seeder_Ender.sql
+++ CUWebinars.Web/App_Data/Orders/SQLScripts/CUWebinarsMigrations/__Seeder_Ender.sql
@@ -4,94 +4,82 @@ SET     BiographyLong = REPLACE(BiographyLong, 'https://ttseast.blob.core.window
         PhotoThumb = REPLACE(PhotoThumb, 'https://ttseast.blob.core.windows.net', 'http://images.ttstrain.com')
 WHERE   idUser > 1
 
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4591, 45 )
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4593, 45 )
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4596, 45 )
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4597, 45 )
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4598, 45 )
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4601, 45 )
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4602, 45 )
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4606, 45 )
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4617, 45 )
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4622, 45 )
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4623, 45 )
 
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 1473, 46 )
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4558, 46 )
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4559, 46 )
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4560, 46 )
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4561, 46 )
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4562, 46 )
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4563, 46 )
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4564, 46 )
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4565, 46 )
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4566, 46 )
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4567, 46 )
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4568, 46 )
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4570, 46 )
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4571, 46 )
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4572, 46 )
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4574, 46 )
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4578, 46 )
-INSERT  CUWebinars.dbo.RegTypesGroupsXref
-        ( idWebinar, idRegTypeGroup )
-VALUES  ( 4580, 46 )
+--SELECT 'INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( '+CAST(idWebinar AS varchar)+', 45)' FROM [CUWebinars-2015-1-7-13-40].dbo.Webinar WHERE Status = 2 
+--SELECT 'INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( '+CAST(idWebinar AS varchar)+', 46)' FROM [CUWebinars-2015-1-7-13-40].dbo.Webinar WHERE Status > 2 
+
+
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4591, 45)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4593, 45)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4596, 45)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4597, 45)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4598, 45)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4601, 45)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4602, 45)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4606, 45)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4617, 45)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4622, 45)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4623, 45)
+
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 400, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 401, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 402, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 403, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 404, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 407, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 435, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 437, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 467, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 701, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 801, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 802, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 803, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 825, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 834, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 1268, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 1285, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 1326, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 1358, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 1410, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 1411, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 1415, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 1416, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 1420, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 1422, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 1426, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 1430, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 1432, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 1433, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 1436, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 1437, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 1438, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 1471, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 1472, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 1473, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 1558, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 3558, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 3559, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4558, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4559, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4560, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4561, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4562, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4563, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4564, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4565, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4566, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4567, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4568, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4570, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4571, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4572, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4574, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4578, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4580, 46)
+INSERT  CUWebinars.dbo.RegTypesGroupsXref ( idWebinar, idRegTypeGroup ) VALUES  ( 4600, 46)
+SET IDENTITY_INSERT CUWebinars.dbo.Webinar off
+
+
 
 SET IDENTITY_INSERT CUWebinars.dbo.Institution ON
 
