26c49b3f033e968aa6e8b9ebe1cc576116cc50b8
Stephen Hueners <steve@juststeve.com>
2015-02-03 05:03:20 -0600

    Begins Affiliate/Admin formatting

--- CUWebinars.Web/App_Data/Orders/MirgrateOrderToWebinar.sql
+++ CUWebinars.Web/App_Data/Orders/MirgrateOrderToWebinar.sql
@@ -1,12 +1,10 @@
 USE TTSWebinars2
 GO
 
---SELECT FirstName, LastName, 'na', 'TTS', REPLACE(email, 'admin', ''), * FROM dbo.USERS WHERE UserType = 4 and email LIKE '%ttstrain.com'
+
 DECLARE @idwebinar INT
 SET @idwebinar = 1712
 
-
---SELECT * INTO '+ GEtdate() 
 
 SELECT  ( SELECT    o.idAffiliate
           FROM      dbo.OrdersRows
@@ -16,7 +14,7 @@ SELECT  ( SELECT    o.idAffiliate
           FROM      dbo.OrdersRows
           WHERE     o.idOrder = idOrder
         ) AS WebinarID ,
-		r.registrationType AS idRegType,
+        r.registrationType AS idRegType ,
         o.firstName AS FirstName ,
         o.lastName AS LastName ,
         '' AS Title ,
@@ -29,39 +27,38 @@ SELECT  ( SELECT    o.idAffiliate
                                                 AND idUser = o.idUser
                                       )) AS Institution ,
         --REPLACE(o.email, '@', '@NN') AS Email ,
-		email AS Email,
+        email AS Email ,
         o.phone AS Phone ,
         o.address AS Address ,
-        'na' AS Address2 ,
+        '' AS Address2 ,
         o.city AS City ,
         ISNULL(o.state, 'na') AS State ,
         o.zip AS Zip ,
         '' AS DiscountCode ,--r.idDiscount ,
-        ( (SELECT   ISNULL(additional_locations_emails, '')
-           FROM     dbo.OrdersRowsOptions
-           WHERE    r.idOrderRow = idOrderRow)
+        ( SELECT    REPLACE(ISNULL(additional_locations_emails, ''), 'NULL', '')
+          FROM      dbo.OrdersRowsOptions
+          WHERE     r.idOrderRow = idOrderRow
         ) AS AdditionalLocations ,
-        o.shippingFirstName ,
-        o.shippingLastName ,
-        o.shippingPhone ,
-        o.shippingAddress ,
-        o.shippingCity ,
-        o.shippingState ,
-        o.shippingZip,
-		o.total,
-		r.shipmentDate,
-		o.storeCommentsPriv,
-		o.idOrder,
-		o.orderDate,
-		r.status
+        REPLACE(ISNULL(o.shippingFirstName, o.firstName), 'NULL', o.firstName) ,
+        REPLACE(ISNULL(o.shippingLastName, o.lastName), 'NULL', o.lastName) ,
+        REPLACE(ISNULL(o.shippingPhone, o.phone), 'NULL', o.phone) ,
+        REPLACE(ISNULL(o.shippingAddress, o.address), 'NULL', o.address) ,
+        REPLACE(ISNULL(o.shippingCity, o.city), 'NULL', o.city) ,
+        REPLACE(ISNULL(o.shippingState, o.state), 'NULL', o.state) ,
+        REPLACE(ISNULL(o.shippingZip, o.zip), 'NULL', o.zip) ,
+        o.total ,
+        r.shipmentDate ,
+        o.storeCommentsPriv ,
+        o.idOrder ,
+        o.orderDate ,
+        r.status
 FROM    TTSWebinars2.dbo.Orders o
         INNER JOIN dbo.OrdersRows r ON r.idOrder = o.idOrder
 WHERE   r.idWebinar = @idwebinar
         AND ( r.status < 5
               AND r.status > 1
             )
-
-ORDER BY orderDate desc
+ORDER BY orderDate DESC
 GO
 
 
