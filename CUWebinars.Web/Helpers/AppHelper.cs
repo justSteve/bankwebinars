da57807247f15c051aa3f392b7013806734c5476
Stephen Hueners <steve@juststeve.com>
2015-02-05 19:43:30 -0600

    Begins Admin Page formatting

--- CUWebinars.Web/Helpers/AppHelper.cs
+++ CUWebinars.Web/Helpers/AppHelper.cs
@@ -19,6 +19,18 @@ namespace CUWebinars.Web.Helpers
         {
             _request = request;
         }
+        public static IEnumerable<int> StringToIntList(string str)
+        {
+            if (String.IsNullOrEmpty(str))
+                yield break;
+
+            foreach (var s in str.Split(','))
+            {
+                int num;
+                if (int.TryParse(s, out num))
+                    yield return num;
+            }
+        }
 
         /// <summary>
         /// Retrieves the city state zip via URL parameter and stored in the ASP.NET Session
