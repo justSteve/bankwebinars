a9a9705b19fcb0a2f3b7822c7c2cc4e591f34af7
David Rogers <davidrogersau@yahoo.com.au>
2015-02-11 16:05:29 +1030

    Fixed UpdateConnectionInfo modal.

--- CUWebinars.Web/Models/ConnectionInfoModel.cs
+++ CUWebinars.Web/Models/ConnectionInfoModel.cs
@@ -1,22 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
-using CUWebinars.Business.Models;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace CUWebinars.Web.Models
 {
-    public class ConnectionInfoModel
+    public class ConnectionInfoModel : ConnectionInfoEditModel
     {
-
-        public int idWebinar { get; set; }
-
         [Display(Name = "Citrix Key")]
         public string WebinarKey { get; set; }
-        public string OrganizerKey { get; set; }
-        public string OrganizerOAuthKey { get; set; }
-        public string ManageURL { get; set; }
         public string CitrixRegisterURL { get; set; }
 
         [Display(Name = "Webinar Phone Number")]
@@ -27,8 +16,5 @@ namespace CUWebinars.Web.Models
         public string AccessCodePresenter { get; set; }
         [Display(Name = "Organizer Code")]
         public string AccessCodeOrganizer { get; set; }
-
-        public ICollection<WebinarFile> WebinarFiles { get; set; }
-
     }
 }
