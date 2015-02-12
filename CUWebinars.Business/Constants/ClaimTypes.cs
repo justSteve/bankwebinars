dc64e35134902672dd0a7a1a3f949ff49f6de665
Stephen Hueners <steve@juststeve.com>
2015-02-12 01:23:40 -0600

    Additional Admin page work

--- CUWebinars.Business/Constants/ClaimTypes.cs
+++ CUWebinars.Business/Constants/ClaimTypes.cs
@@ -8,8 +8,7 @@ namespace CUWebinars.Business.Constants
         public static readonly string Affiliate = "http://bankwebinars.com/ws/2014/01/identity/claims/Affiliate";
         public static readonly string FullName = "http://cuwebinars.com/ws/2014/01/identity/claims/FullName";
         public static readonly string HasNotVerified = "http://ttstrain.com/ws/2014/01/identity/claims/HasNotVerified";
-        public static readonly string ShouldSeeHandouts = "http://ttstrain.com/ws/2014/01/identity/claims/ShouldSeeHandouts";
-        public static readonly string ShouldSeeRecording = "http://ttstrain.com/ws/2014/01/identity/claims/ShouldSeeRecording";
+        public static readonly string DisplayPostEventMaterials = "http://ttstrain.com/ws/2014/01/identity/claims/DisplayPostEventMaterials";
         public static readonly string ShouldSeeCopyOfConnectionInfo = "http://ttstrain.com/ws/2014/01/identity/claims/ShouldSeeCopyOfConnectionInfo";
     }
 }
