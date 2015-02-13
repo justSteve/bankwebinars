bd653bb74ab530fea46d57d30639d1b9e50dfe4d
Stephen Hueners <steve@juststeve.com>
2015-02-13 05:52:00 -0600

    Recording is posted template update

---
+++ CUWebinars.Web/ViewModel/PostEventPublishModel.cs
@@ -0,0 +1,14 @@
+﻿using CUWebinars.Business.Core;
+using CUWebinars.Business.Models;
+
+namespace CUWebinars.Web.ViewModel
+{
+    public class PostEventPublishModel
+    {
+        public Order Order { get; set; }
+        
+        //public OrderGenesis OrderGenesis { get; set; }
+        //public bool UserCreatedInCart { get; set; }
+        //public bool UserCreatedOnImport { get; set; }
+    }
+}
