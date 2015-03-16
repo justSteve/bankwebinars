86eaa800323fdf26330ab3c32d43a457fe7816bb
David Rogers <davidrogersau@yahoo.com.au>
2015-03-16 15:59:25 +1030

    Added validator for updating webinar.

--- CUWebinars.Business/Validation/Webinar/CreateWebinarValidator.cs
+++ CUWebinars.Business/Validation/Webinar/CreateWebinarValidator.cs
@@ -1,51 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using CUWebinars.Business.Models;
-using FluentValidation;
+﻿using FluentValidation;
 
 namespace CUWebinars.Business.Validation.Webinar
 {
-    public class CreateWebinarValidator : AbstractValidator<Models.Webinar>
+    public class CreateWebinarValidator : UpdateWebinarValidator
     {
-        private const string CannotBeNullOrEmpty = "The {0} cannot be null or empty";
-        private const decimal MinDecimal = 0.25M;
-        private const string MustBeGreaterThan = "The {0} must be greater than {1}";
-
         public CreateWebinarValidator()
         {
-            // strings
-            RuleFor(w => w.Title).NotEmpty().WithMessage(CannotBeNullOrEmpty, "Title");
-            RuleFor(w => w.Description).NotEmpty().WithMessage(CannotBeNullOrEmpty, "Description");
-            RuleFor(w => w.DescriptionLong).NotEmpty().WithMessage(CannotBeNullOrEmpty, "LongDescription");
-            RuleFor(w => w.LearnBody).NotEmpty().WithMessage(CannotBeNullOrEmpty, "LearnBody");
-            RuleFor(w => w.LearnCaption).NotEmpty().WithMessage(CannotBeNullOrEmpty, "LearnCaption");
-            RuleFor(w => w.WhoAttend).NotEmpty().WithMessage(CannotBeNullOrEmpty, "WhoAttend");
             RuleFor(w => w.RecordingUrl).NotEmpty().WithMessage(CannotBeNullOrEmpty, "RecordingUrl");
-
-            // dates
-            RuleFor(w => w.Date).NotEmpty().WithMessage(CannotBeNullOrEmpty, "Date");
-            RuleFor(w => w.DateChanged).NotEmpty().WithMessage(CannotBeNullOrEmpty, "DateChanged");
-            RuleFor(w => w.DateCreated).NotEmpty().WithMessage(CannotBeNullOrEmpty, "DateCreated");
-
-            // numbers
-            RuleFor(w => w.idPresenter).GreaterThan(0).WithMessage(MustBeGreaterThan, "SelectedPresenter", 0);
-            RuleFor(w => w.Duration).GreaterThan(d => MinDecimal).WithMessage(MustBeGreaterThan, "Duration", MinDecimal);
-            RuleFor(w => (int)w.Status).GreaterThan(0).WithMessage(MustBeGreaterThan, "SelectedStatus", 0);
-
-            // items which are lookups
-            RuleFor(w => w.RegTypesGroupsXref).Must(MustHaveAtLeastOneRegTypeGroup).WithMessage("Webinar must have at least 1 RegType");
-            RuleFor(w => w.WebinarTopicXrefs).Must(MustHaveAtLeastOneTopic).WithMessage("Webinar must have at least 1 topic");
-        }
-
-        private bool MustHaveAtLeastOneRegTypeGroup(ICollection<RegTypesGroupsXref> regTypesGroupsXrefs)
-        {
-            return regTypesGroupsXrefs.Any();
-        }
-
-        private bool MustHaveAtLeastOneTopic(ICollection<WebinarTopicXref> webinarTopicXrefs)
-        {
-            return webinarTopicXrefs.Any();
         }
     }
 }
---
+++ CUWebinars.Business/Validation/Webinar/UpdateWebinarValidator.cs
@@ -0,0 +1,50 @@
+﻿using CUWebinars.Business.Models;
+using FluentValidation;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CUWebinars.Business.Validation.Webinar
+{
+    public class UpdateWebinarValidator : AbstractValidator<Models.Webinar>
+    {
+        protected const string CannotBeNullOrEmpty = "The {0} cannot be null or empty";
+        protected const decimal MinDecimal = 0.25M;
+        protected const string MustBeGreaterThan = "The {0} must be greater than {1}";
+
+        public UpdateWebinarValidator()
+        {
+            // strings
+            RuleFor(w => w.Title).NotEmpty().WithMessage(CannotBeNullOrEmpty, "Title");
+            RuleFor(w => w.Description).NotEmpty().WithMessage(CannotBeNullOrEmpty, "Description");
+            RuleFor(w => w.DescriptionLong).NotEmpty().WithMessage(CannotBeNullOrEmpty, "LongDescription");
+            RuleFor(w => w.LearnBody).NotEmpty().WithMessage(CannotBeNullOrEmpty, "LearnBody");
+            RuleFor(w => w.LearnCaption).NotEmpty().WithMessage(CannotBeNullOrEmpty, "LearnCaption");
+            RuleFor(w => w.WhoAttend).NotEmpty().WithMessage(CannotBeNullOrEmpty, "WhoAttend");
+
+            // dates
+            RuleFor(w => w.Date).NotEmpty().WithMessage(CannotBeNullOrEmpty, "Date");
+            RuleFor(w => w.DateChanged).NotEmpty().WithMessage(CannotBeNullOrEmpty, "DateChanged");
+            RuleFor(w => w.DateCreated).NotEmpty().WithMessage(CannotBeNullOrEmpty, "DateCreated");
+
+            // numbers
+            RuleFor(w => w.idPresenter).GreaterThan(0).WithMessage(MustBeGreaterThan, "SelectedPresenter", 0);
+            RuleFor(w => w.Duration).GreaterThan(d => MinDecimal).WithMessage(MustBeGreaterThan, "Duration", MinDecimal);
+            RuleFor(w => (int)w.Status).GreaterThan(0).WithMessage(MustBeGreaterThan, "SelectedStatus", 0);
+
+            // items which are lookups
+            RuleFor(w => w.RegTypesGroupsXref).Must(MustHaveAtLeastOneRegTypeGroup).WithMessage("Webinar must have at least 1 RegType");
+            RuleFor(w => w.WebinarTopicXrefs).Must(MustHaveAtLeastOneTopic).WithMessage("Webinar must have at least 1 topic");
+        }
+
+        protected virtual bool MustHaveAtLeastOneRegTypeGroup(ICollection<RegTypesGroupsXref> regTypesGroupsXrefs)
+        {
+            return regTypesGroupsXrefs.Any();
+        }
+
+        protected virtual bool MustHaveAtLeastOneTopic(ICollection<WebinarTopicXref> webinarTopicXrefs)
+        {
+            return webinarTopicXrefs.Any();
+        }
+
+    }
+}
