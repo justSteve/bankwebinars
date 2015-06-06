1392f90aa9e3780950cbf5bf6b337364813dbc50
David Rogers <davidalanrogers@live.com.au>
2015-06-06 17:41:40 +0930

    Added Orders datatable into slider panel of the DetailsAffilate page.

---
+++ CUWebinars.Business/Services/IDataTablesService.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using CUWebinars.Business.Models;
+
+namespace CUWebinars.Business.Services
+{
+    public interface IDataTablesService
+    {
+        IEnumerable<Order> GetAllOrders();
+        IEnumerable<Order> GetOrdersByWebinar(int idWebinar, int idAffliate, out int totalNumberOrders);
+        IEnumerable<Order> GetOrdersByUser(int idUser, int idAffliate, int start, int length, out int totalNumberOrders);
+        IEnumerable<Order> GetOrdersPaged(int start, int length, string orderIdFragment, out int totalNumberOrders, out int totalFilteredOrders);
+    }
+}
