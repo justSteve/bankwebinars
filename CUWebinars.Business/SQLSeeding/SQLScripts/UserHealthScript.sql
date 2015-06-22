0eccef49cd5cf0742ba31a5eacfdca62940a3347
steve <steve@juststeve.com>
2015-06-22 14:26:46 -0500

    Added ID to divs for sake of GI

--- CUWebinars.Business/SQLSeeding/SQLScripts/UserHealthScript.sql
+++ CUWebinars.Business/SQLSeeding/SQLScripts/UserHealthScript.sql
@@ -2,80 +2,62 @@
 /* show UserAccount records which do not appear to be properly verified */
 /************************************************************************************************************/
 
-SELECT
-	[Key], IsAccountVerified,VerificationKey, VerificationPurpose, VerificationKeySent, VerificationStorage
-FROM
-	UserAccounts
-WHERE
-	IsAccountVerified = 0
-OR	
-	VerificationKey IS NOT NULL
-OR	
-	VerificationPurpose IS NOT NULL
-OR	
-	VerificationKeySent IS NOT NULL
-OR	
-	VerificationStorage IS NOT NULL
+SELECT  [Key] ,
+        IsAccountVerified ,
+        VerificationKey ,
+        VerificationPurpose ,
+        VerificationKeySent ,
+        VerificationStorage
+FROM    UserAccounts
+WHERE   IsAccountVerified = 0
+        OR VerificationKey IS NOT NULL
+        OR VerificationPurpose IS NOT NULL
+        OR VerificationKeySent IS NOT NULL
+        OR VerificationStorage IS NOT NULL
 
 /************************************************************************************************************/
 /* show UserAccount records with a 'troubled' history */
 /************************************************************************************************************/
-SELECT
-	*
-FROM
-	UserAccounts
-WHERE
-	LastFailedLogin IS NOT NULL
-OR
-	FailedLoginCount > 0
-OR
-	IsAccountClosed > 0
-OR
-	AccountClosed IS NOT NULL
+SELECT  *
+FROM    UserAccounts
+WHERE   LastFailedLogin IS NOT NULL
+        OR FailedLoginCount > 0
+        OR IsAccountClosed > 0
+        OR AccountClosed IS NOT NULL
 
 /************************************************************************************************************/
 /* show UserAccounts records which do not have a FullName claim or a WebUser claim */
 /************************************************************************************************************/
-SELECT
-	*
-FROM
-	UserAccounts ua
-WHERE
-	NOT EXISTS (SELECT 
-					* 
-				FROM 
-					UserClaims uc	
-				WHERE 
-					uc.ParentKey = ua.[Key] 
-				AND [Type] = 'http://cuwebinars.com/ws/2014/01/identity/claims/FullName')
-OR
-	NOT EXISTS (SELECT 
-					* 
-				FROM 
-					UserClaims uc	
-				WHERE 
-					uc.ParentKey = ua.[Key] 
-				AND [Type] = 'http://schemas.microsoft.com/ws/2008/06/identity/claims/role')
+SELECT  *
+FROM    UserAccounts ua
+WHERE   NOT EXISTS ( SELECT *
+                     FROM   UserClaims uc
+                     WHERE  uc.ParentKey = ua.[Key]
+                            AND [Type] = 'http://cuwebinars.com/ws/2014/01/identity/claims/FullName' )
+        OR NOT EXISTS ( SELECT  *
+                        FROM    UserClaims uc
+                        WHERE   uc.ParentKey = ua.[Key]
+                                AND [Type] = 'http://schemas.microsoft.com/ws/2008/06/identity/claims/role' )
 
 /************************************************************************************************************/
 /* show WebUser records which do not have corresponding UserAccount records */
 /************************************************************************************************************/
-SELECT 
-	wu.email, wu.idUser -- add any more cols desired
-FROM 
-	WebUser wu
-WHERE
-	NOT EXISTS (SELECT * FROM UserAccounts ua WHERE ua.Email = wu.Email)
+SELECT  wu.email ,
+        wu.idUser -- add any more cols desired
+FROM    WebUser wu
+WHERE   NOT EXISTS ( SELECT *
+                     FROM   UserAccounts ua
+                     WHERE  ua.Email = wu.email )
 
 /************************************************************************************************************/
 /* show UserAccount records which do not have corresponding WebUser records */
 /************************************************************************************************************/
-SELECT 
-	ua.email, ua.[Key] -- add any more cols desired
-FROM
-	UserAccounts ua
-WHERE
-	NOT EXISTS (SELECT * FROM WebUser wu WHERE wu.email = ua.Email)
+SELECT  ua.Email ,
+        ua.[Key] -- add any more cols desired
+FROM    UserAccounts ua
+WHERE   NOT EXISTS ( SELECT *
+                     FROM   WebUser wu
+                     WHERE  wu.email = ua.Email )
 
 
 /************************************************************************************************************/
@@ -83,9 +65,7 @@ WHERE
  *	taken to retain the record/row for problem orders, until the issue with that order has been resolved. */
 /************************************************************************************************************/
 
-SELECT 
-	wu.email, wu.idUser -- add any more cols desired
-FROM 
-	WebUser wu
-WHERE
-	Email like '%@notauthenticated.com'
+SELECT  wu.email ,
+        wu.idUser -- add any more cols desired
+FROM    WebUser wu
+WHERE   email LIKE '%@notauthenticated.com'
