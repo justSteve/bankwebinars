1b5b6d731d03d094313bd02d5077347fe8e1157b
steve <steve@juststeve.com>
2015-08-26 14:02:24 -0500

    Basic Selenium functionality for CBT completed

--- CUWebinars.CitrixDriver/Program.cs
+++ CUWebinars.CitrixDriver/Program.cs
@@ -1,127 +1,52 @@
-﻿using CUWebinars.Business.Core.Helpers;
-using CUWebinars.Business.Models;
-using KesselRun.SeleniumCore.Enums;
-using KesselRun.SeleniumCore.Infrastructure;
-using KesselRun.SeleniumCore.Infrastructure.Factories;
-using KesselRun.SeleniumCore.Infrastructure.Factories.Contracts;
-using System;
-using System.Configuration;
+﻿// Please visit http://docs.seleniumhq.org/docs/03_webdriver.jsp#getting-started-with-webdriver for detailed installation and instructions
+//Quick start video tutorial using Visual Studio: https://www.youtube.com/watch?v=CxDkRJ1iHwE
+//Step-by-step video turoial using Visual Studio: https://www.youtube.com/watch?v=uRJL0zu7U6k
 
-namespace CUWebinars.CitrixDriver
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Drawing;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+
+namespace SimpleCSharpSelenium2
 {
     class Program
     {
-        private const string Attendee = "Attendee";
-        private const string Organizer = "Organizer";
-        private const string Panelist = "Panelist";
-
-        private static KesselRun.SeleniumCore.TestDrivers.Contracts.ITestDriver webDriver;
-        private static CitrixWebPage citrixWebPage;
-        private static TTSWebinarsContext context = new TTSWebinarsContext();
-        private static DateTimeHelper dateTimeHelper = new DateTimeHelper();
-
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Green; // love the old green screen!
+            DesiredCapabilities caps = new DesiredCapabilities();
 
-            var ttsWebinar = GetWebinar();
+            caps.SetCapability("name", "IE8 Select Radio");
+            caps.SetCapability("build", "1.0");
+            caps.SetCapability("browser_api_name", "IE8");
+            caps.SetCapability("os_api_name", "Win7-C1");
+            caps.SetCapability("screen_resolution", "1024x768");
+            caps.SetCapability("record_video", "true");
+            caps.SetCapability("record_network", "true");
+            caps.SetCapability("record_snapshot", "false");
 
-            var webinar = GetGTWebinar(ttsWebinar);
+            caps.SetCapability("username", "steve@ttstrain.com");
+            caps.SetCapability("password", "u4dd2077d8c398e7");
 
-            if (webinar != null)
-            {
-                ITestDriverFactory foundry = new TestDriverFactory(new DriverOptions
-                {
-                    DriverEngine = DriverEngine.AutoDetect,
-                    DriverExePath = @ConfigurationManager.AppSettings["FireFoxBinaryPath"],
-                    Port = 8889
-                });
+            IWebDriver driver = new RemoteWebDriver(new Uri("http://hub.crossbrowsertesting.com:80/wd/hub"), caps);
 
-                webDriver = foundry.CreateTestDriver(DriverType.Firefox);
+            Console.WriteLine("Loading Url");
+            driver.Navigate().GoToUrl("http://v3.bankwebinars.com/1893/all-about-the-new-ffiec-cybersecurity-assessment-tool");
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until((d) => { return d.Title.ToLower().StartsWith("all"); });
 
-                citrixWebPage = new CitrixWebPage(webDriver, ConfigurationManager.AppSettings["HomeUrl"]);
+            driver.FindElement(By.Id("regTypeID_208")).Click();
+            Thread.Sleep(8000);
+            //maximize the window - DESKTOPS ONLY
+            //Console.WriteLine("Maximizing window");
+            //driver.Manage().Window.Size = new Size(1024,768);
 
-                //  Open the page for login details
-                citrixWebPage.Open();
-
-                //  Login, using the credentials in App.config
-                Login();
-
-                citrixWebPage.GoToWebinarsPage();
-
-                var newWebinarKey = ScheduleASimilarWebinar(webinar);
-
-                ttsWebinar.AccessCodeAttendee = newWebinarKey.AttendeeAccessCode;
-                ttsWebinar.AccessCodeOrganizer = newWebinarKey.OrganizerAccessCode;
-                ttsWebinar.AccessCodePresenter = newWebinarKey.PanelistAccessCode;
-
-                ttsWebinar.AccessPhone = newWebinarKey.AttendeePhoneNr;
-                
-                ttsWebinar.WebinarKey = newWebinarKey.WebinarId;
-                ttsWebinar.ConnectionInfo = newWebinarKey.WebinarUrl;
-
-                context.SaveChanges();
-
-                citrixWebPage.Close();
-
-                Console.WriteLine("{0} Press any key to close...", Environment.NewLine);
-                Console.ReadLine();
-            }
-        }
-
-        private static Webinar GetWebinar()
-        {
-            Console.Write("Enter Webinar Number: ");
-            var textEntered = Console.ReadLine();
-            int webinarId;
-            while (!int.TryParse(textEntered, out webinarId))
-            {
-                Console.Write("Invalid input. The Webinar Number must be a valid Integer: ");
-                textEntered = Console.ReadLine();
-            }
-
-            var webinar = context.Webinars.Find(webinarId);
-
-            return webinar;
-        }
-
-        private static GTWebinar GetGTWebinar(Webinar webinar)
-        {
-            var GTWebinar = new GTWebinar
-            {
-                //StartTime = DateTime.Now.AddMonths(2),
-                StartTime = webinar.Date,
-                StartHour = webinar.Date.ToString("hh:mm").ToLower(),
-                StartMeridian = webinar.Date.ToString("tt"),
-                EndHour = webinar.Date.AddHours((double)webinar.Duration).ToString("hh:mm"),
-                EndMeridian = webinar.Date.ToString("tt"),
-                PresenterFirstName =  webinar.Presenter.WebUser.FirstName,
-                PresenterLastName=  webinar.Presenter.WebUser.LastName,
-                RequirePassword = true,
-                TemplateTitle = ConfigurationManager.AppSettings["TemplateTitle"],
-                TimeZoneKey = 68,
-                Description = webinar.Description,
-                Title = webinar.Title // central
-            };
-
-            return GTWebinar;
-        }
-
-        private static void Login()
-        {
-            var userName = ConfigurationManager.AppSettings["UserName"];
-            var passWord = ConfigurationManager.AppSettings["Password"];
-
-            citrixWebPage.GotToLoginPage();
-            citrixWebPage.EnterEmailAddress(userName);
-            citrixWebPage.EnterPasswordWhereUserExists(passWord);
-            citrixWebPage.ClickSubmit();
-        }
-
-        private static WebinarDetails ScheduleASimilarWebinar(GTWebinar webinar)
-        {
-            
-            return citrixWebPage.ScheduleASimilarWebinar(webinar);
+            driver.Quit();
 
         }
     }
---
+++ CUWebinars.CitrixDriver/ProgramCITRIX.cs
@@ -0,0 +1,128 @@
+﻿using CUWebinars.Business.Core.Helpers;
+using CUWebinars.Business.Models;
+using KesselRun.SeleniumCore.Enums;
+using KesselRun.SeleniumCore.Infrastructure;
+using KesselRun.SeleniumCore.Infrastructure.Factories;
+using KesselRun.SeleniumCore.Infrastructure.Factories.Contracts;
+using System;
+using System.Configuration;
+
+namespace CUWebinars.CitrixDriver
+{
+    class ProgramCITRIX
+    {
+        private const string Attendee = "Attendee";
+        private const string Organizer = "Organizer";
+        private const string Panelist = "Panelist";
+
+        private static KesselRun.SeleniumCore.TestDrivers.Contracts.ITestDriver webDriver;
+        private static CitrixWebPage citrixWebPage;
+        private static TTSWebinarsContext context = new TTSWebinarsContext();
+        private static DateTimeHelper dateTimeHelper = new DateTimeHelper();
+
+        static void MainCITRIX(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green; // love the old green screen!
+
+            var ttsWebinar = GetWebinar();
+
+            var webinar = GetGTWebinar(ttsWebinar);
+
+            if (webinar != null)
+            {
+                ITestDriverFactory foundry = new TestDriverFactory(new DriverOptions
+                {
+                    DriverEngine = DriverEngine.AutoDetect,
+                    DriverExePath = @ConfigurationManager.AppSettings["FireFoxBinaryPath"],
+                    Port = 8889
+                });
+
+                webDriver = foundry.CreateTestDriver(DriverType.Firefox);
+
+                citrixWebPage = new CitrixWebPage(webDriver, ConfigurationManager.AppSettings["HomeUrl"]);
+
+                //  Open the page for login details
+                citrixWebPage.Open();
+
+                //  Login, using the credentials in App.config
+                Login();
+
+                citrixWebPage.GoToWebinarsPage();
+
+                var newWebinarKey = ScheduleASimilarWebinar(webinar);
+
+                ttsWebinar.AccessCodeAttendee = newWebinarKey.AttendeeAccessCode;
+                ttsWebinar.AccessCodeOrganizer = newWebinarKey.OrganizerAccessCode;
+                ttsWebinar.AccessCodePresenter = newWebinarKey.PanelistAccessCode;
+
+                ttsWebinar.AccessPhone = newWebinarKey.AttendeePhoneNr;
+                
+                ttsWebinar.WebinarKey = newWebinarKey.WebinarId;
+                ttsWebinar.ConnectionInfo = newWebinarKey.WebinarUrl;
+
+                context.SaveChanges();
+
+                citrixWebPage.Close();
+
+                Console.WriteLine("{0} Press any key to close...", Environment.NewLine);
+                Console.ReadLine();
+            }
+        }
+
+        private static Webinar GetWebinar()
+        {
+            Console.Write("Enter Webinar Number: ");
+            var textEntered = Console.ReadLine();
+            int webinarId;
+            while (!int.TryParse(textEntered, out webinarId))
+            {
+                Console.Write("Invalid input. The Webinar Number must be a valid Integer: ");
+                textEntered = Console.ReadLine();
+            }
+
+            var webinar = context.Webinars.Find(webinarId);
+
+            return webinar;
+        }
+
+        private static GTWebinar GetGTWebinar(Webinar webinar)
+        {
+            var GTWebinar = new GTWebinar
+            {
+                //StartTime = DateTime.Now.AddMonths(2),
+                StartTime = webinar.Date,
+                StartHour = webinar.Date.ToString("hh:mm").ToLower(),
+                StartMeridian = webinar.Date.ToString("tt"),
+                EndHour = webinar.Date.AddHours((double)webinar.Duration).ToString("hh:mm"),
+                EndMeridian = webinar.Date.ToString("tt"),
+                PresenterFirstName =  webinar.Presenter.WebUser.FirstName,
+                PresenterLastName=  webinar.Presenter.WebUser.LastName,
+                RequirePassword = true,
+                TemplateTitle = ConfigurationManager.AppSettings["TemplateTitle"],
+                TimeZoneKey = 68,
+                Description = webinar.Description,
+                Title = webinar.Title // central
+            };
+
+            return GTWebinar;
+        }
+
+        private static void Login()
+        {
+            var userName = ConfigurationManager.AppSettings["UserName"];
+            var passWord = ConfigurationManager.AppSettings["Password"];
+
+            citrixWebPage.GotToLoginPage();
+            citrixWebPage.EnterEmailAddress(userName);
+            citrixWebPage.EnterPasswordWhereUserExists(passWord);
+            citrixWebPage.ClickSubmit();
+        }
+
+        private static WebinarDetails ScheduleASimilarWebinar(GTWebinar webinar)
+        {
+            
+            return citrixWebPage.ScheduleASimilarWebinar(webinar);
+
+        }
+    }
+}
