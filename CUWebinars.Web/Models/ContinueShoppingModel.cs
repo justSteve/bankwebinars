63ac845e051c45f450a200607fb4fdff1a7f4120
Steve <steve@juststeve.com>
2016-10-02 04:21:28 -0500

    Project opened via VS15 update 3 which detects changes in the commited files even though no changes have been made via normal editing.

---
+++ CUWebinars.Web/Models/ContinueShoppingModel.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using CUWebinars.Business.Models;
+
+namespace CUWebinars.Web.Models
+{
+    public class ContinueShoppingModel
+    {
+        public IEnumerable<Webinar> SelectedRelated { get; set; }
+        public IEnumerable<Webinar> SelectedPresenter { get; set; }
+        public IEnumerable<Webinar> SelectedTopics { get; set; }
+    }
+}
