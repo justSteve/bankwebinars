c0e862f91420c4267df3f70e34c8fe72db3ad416
steve <steve@juststeve.com>
2016-10-03 14:42:08 -0500

    These files picked up as 'added' or 'changed' when opening project via 2015 on Steve's desktop.

---
+++ CUWebinars.Web/Scripts/app/ToastLogger.js
@@ -0,0 +1,66 @@
+﻿/// <reference path="../typings/jquery/jquery.d.ts" />
+/// <reference path="../typings/toastr/toastr.d.ts" />
+var Common;
+(function (Common) {
+    var Logger = (function () {
+        function Logger() {
+            var _this = this;
+            this.getLogFn = function (moduleId, fnName) {
+                fnName = fnName || 'log';
+
+                switch (fnName.toLowerCase()) {
+                    case 'success':
+                        fnName = 'logSuccess';
+                        break;
+                    case 'error':
+                        fnName = 'logError';
+                        break;
+                    case 'warn':
+                        fnName = 'logWarning';
+                        break;
+                    case 'warning':
+                        fnName = 'logWarning';
+                        break;
+                }
+
+                var func = _this[fnName];
+
+                return function (msg, data, showToast) {
+                    func(msg, data, moduleId, (showToast === undefined) ? true : showToast);
+                };
+            };
+            this.log = function (message, data, source, showToast) {
+                _this.logIt(message, data, source, showToast, 'info');
+            };
+            this.logWarning = function (message, data, source, showToast) {
+                _this.logIt(message, data, source, showToast, 'warning');
+            };
+            this.logSuccess = function (message, data, source, showToast) {
+                _this.logIt(message, data, source, showToast, 'success');
+            };
+            this.logError = function (message, data, source, showToast) {
+                _this.logIt(message, data, source, showToast, 'error');
+            };
+        }
+        Logger.prototype.logIt = function (message, data, source, showToast, toastType) {
+            source = source ? '[' + source + '] ' : '';
+            console.info(source, message, data || '');
+
+            if (showToast) {
+                if (toastType === 'error') {
+                    toastr.error(message);
+                } else if (toastType === 'warning') {
+                    toastr.warning(message);
+                } else if (toastType === 'success') {
+                    toastr.success(message);
+                } else {
+                    toastr.info(message);
+                }
+            }
+        };
+        return Logger;
+    })();
+    Common.Logger = Logger;
+    ;
+})(Common || (Common = {}));
+//# sourceMappingURL=ToastLogger.js.map
---
+++ CUWebinars.Web/Scripts/app/admin/affiliateReport.js
@@ -0,0 +1,205 @@
+﻿var affiliateReportTable;
+var listOfAffiliates = new Array();
+var idWebinar;
+
+function sendAll() {
+
+    for (var Aff in listOfAffiliates) {
+        sendReport(idWebinar, listOfAffiliates[Aff]);
+
+    }
+
+    this.location.href = "/Admin";
+}
+
+
+//function initAffiliateReport(tableId, options) {
+
+//    // default settings, will be overriden by values from options
+//    royaltiesTable.dataTable({
+//        "bProcessing": true,
+//        "bFilter": false,
+//        "bPaginate": false,
+//        "bServerSide": true,
+//        "bStateSave": true,
+//        "aoColumns": [
+//            { "sWidth": "5%", "bSortable": false },
+//            {
+//                "bSortable": false, "sWidth": "5%",
+//                "fnRender": function (obj) {
+//                    var orderData = obj.aData[obj.iDataColumn];
+
+//                    listOfAffiliates.push(orderData.affiliateID);
+
+//                    idWebinar = orderData.webinarID;
+//                    return '<button name="sendReportTo ' + orderData.affiliateID + '" onclick="sendReport(' + orderData.webinarID + ', ' + orderData.affiliateID + '); return false;">Send</button>';
+//                }
+//            },
+//            { "sWidth": "45%", "bSortable": false },
+//            { "sWidth": "10%", "bSortable": false },
+//            { "sWidth": "25%", "bSortable": false },
+//            {
+//                "sWidth": "5%",
+//                "fnRender": function (obj) {
+//                    var html = obj.aData[obj.iDataColumn];
+//                    if (obj.aData[obj.iDataColumn] != "0")
+//                        html += "&nbsp;<img src='/content/images/add.png' class='toggleRegistrations' id='toggleRegistrations" + obj.aData[0] + "' />";
+
+//                    return html;
+//                },
+//                "bSortable": false
+//            }
+//        ],
+//        "fnServerData": function (sSource, aoData, fnCallback) {
+//            $.ajax({
+//                "dataType": "json",
+//                "type": "POST",
+//                "url": sSource,
+//                "data": aoData,
+//                "success": fnCallback
+//            });
+//        },
+//        "fnDrawCallback": function () {
+//            //initAffiliateRegistrations();
+//            alert("ini");
+//        }
+//    };
+
+//    //jQuery.extend(settings, options);
+//    affiliateReportTable = $("#" + tableId).dataTable(settings);
+
+//    return affiliateReportTable;
+//}
+
+////function getSelectedWebinarID() {
+
+////    return 2083;
+////}
+
+////function refreshWebinarTitle() {
+////    webinarID = getSelectedWebinarID();
+////    if (webinarID != null) {
+////        $.post('/admin/webinars/webinartitle/' + webinarID, function (data) {
+////            $("#selectedWebinarName").html('<h2>' + data + '</h2>');
+////        });
+////    }
+////}
+
+////function sendReport(webinarID, affiliateID) {
+
+////    $.ajax(
+////        {
+////            type: "POST",
+////            url: "/Admin/Reports/SendAffiliateReport",
+////            data: "webinarID=" + webinarID + "&affiliateID=" + affiliateID,
+////            cache: false,
+////            async: false,
+////            success: function (result) {
+////                alert("Report sent to the affiliate #: " + affiliateID);
+////            },
+////            error: function (req, status, error) {
+////                alert("Unable to send the report to the affiliate. Please contact the administrator.");
+////            }
+////        });
+////}
+
+////function initAffiliateRegistrations() {
+////    $("img.toggleRegistrations").click(function () {
+////        var nTr = this.parentNode.parentNode;
+////        if (this.src.match("delete")) {
+////            this.src = "/content/images/add.png";
+////            affiliateReportTable.fnClose(nTr);
+////        }
+////        else {
+////            this.src = "/content/images/delete.png";
+
+////            // getting the affiliate ID from parent row
+////            var iIndex = affiliateReportTable.fnGetPosition(nTr);
+////            var aData = affiliateReportTable.fnSettings().aoData[iIndex]._aData;
+////            var affiliateID = aData[0];
+
+////            affiliateReportTable.fnOpen(nTr, getRegistrationsTableCode(affiliateID), "registrationsTableContainer");
+////            affiliateReportTable.fnSettings().aoOpenRows.push({
+////                "nTr": $(nTr).next()[0],
+////                "nParent": nTr
+////            });
+////            webinarID = getSelectedWebinarID();
+////            initRegistrationsTable(affiliateID, webinarID);
+////        }
+////    });
+////}
+
+////function getRegistrationsTableCode(affiliateID) {
+
+////    var template = $("#registrationsTableTemplate")
+////        .clone()
+////        .attr("id", "registrationsTable" + affiliateID)
+////        .show()
+////        .outerHTML();
+
+////    return template;
+////}
+
+//////the subtable is initialized
+////function initRegistrationsTable(affiliateID, webinarID) {
+////    var settings = {
+////        "bProcessing": true,
+////        "bSort": true,
+////        "bPaginate": false,
+////        "bFilter": false,
+////        "bLengthChange": false,
+////        "sAjaxSource": "/admin/reports/affiliatereportsubtabledata?webinarID=" + webinarID + "&affiliateID=" + affiliateID,
+////        "aoColumns": [
+////        //No
+////            { "bSortable": false, "sWidth": "5%" },
+////        //Name
+////            {
+////                "sWidth": "20%",
+////                "fnRender": function (obj) {
+////                    var orderData = obj.aData[obj.iDataColumn];
+////                    columnData = orderData.customerName + "<br/>" + orderData.customerEmail;
+////                    if (orderData.customerInstitution) {
+////                        columnData += "<br/>" + orderData.customerInstitution;
+////                    }
+////                    return columnData;
+////                }
+////            },
+////        //Order
+////            {
+////                "sWidth": "20%",
+////                "fnRender": function (obj) {
+////                    var orderData = obj.aData[obj.iDataColumn];
+////                    return orderData.orderDate + "<br/>" + orderData.registrationType + "<br/>" + orderData.paymentType;
+////                }
+////            },
+////        //Paid
+////            { "bSortable": false, "sWidth": "20%" },
+////        //Commision
+////            {
+////                "sWidth": "20%",
+////                "bSortable": false,
+////                "fnRender": function (obj) {
+////                    var orderData = obj.aData[obj.iDataColumn];
+////                    return orderData.commission; // + "<br/>" + orderData.commissionModel;
+////                }
+////            },
+////        //Status
+////            { "bSortable": false, "sWidth": "20%" }
+////        ]
+////    };
+
+////    $("#registrationsTable" + affiliateID).dataTable(settings);
+////}
+
+////function resendConnectionInfo(link) {
+////    $.post($(link).attr("href"), function (data, status) {
+////        alert("Connection info was resent successfully");
+////    });
+
+////    return false;
+////}
+
+////// one-line, but useful jQuery plugin :)
+////jQuery.fn.outerHTML = function () {
+////    return $('<div>').append(this.eq(0).clone()).html();
+////};
