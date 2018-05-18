c5657073caad277e6950391603f67aa41ba06e04
Stephen Hueners <steve@juststeve.com>
2018-05-18 05:13:20 -0500

    Reverts to legacy 'GetOrdersByWebinar' method Removes some debug Alerts.

--- CUWebinars.Business/Services/OrderManagementService.cs
+++ CUWebinars.Business/Services/OrderManagementService.cs
@@ -1737,15 +1737,15 @@ namespace CUWebinars.Business.Services
 
         public List<Order> GetOrdersByWebinar(int idWebinar)
         {
-            //var orderIds = _webinarRepository.GetV3OrdersIdsByWebinar(idWebinar);
+            var orderIds = _webinarRepository.GetV3OrdersIdsByWebinar(idWebinar);
             //var orders = _webinarRepository.GetV3OrdersByWebinar(idWebinar);
-            return _webinarRepository.GetOrdersByWebinar(idWebinar).ToList();
-            //List<Order> orders = new List<Order>();
-            //foreach (var orderId in orderIds)
-            //{
-            //    orders.Add(_orderRepository.GetOrderById(orderId));
-                
-            //}
+            //return _webinarRepository.GetOrdersByWebinar(idWebinar).ToList();
+            List<Order> orders = new List<Order>();
+            foreach (var orderId in orderIds)
+            {
+                orders.Add(_orderRepository.GetOrderById(orderId));
+
+            }
             //foreach (var _o in orders)
             //{
             //    if (_o != null)
@@ -1759,7 +1759,7 @@ namespace CUWebinars.Business.Services
             //    }
             //}
 
-            //return orders;
+            return orders;
 
         }
 
--- CUWebinars.Web/Scripts/app/cart/details-affiliate.js
+++ CUWebinars.Web/Scripts/app/cart/details-affiliate.js
@@ -1073,7 +1073,7 @@ OCA.wireUpHandlers = function () {
                             $('#createNewUserButton').hide();
 
                             confirmationForAffiliateDiv.load('/cart/CheckoutConfirmForAffiliate/' + OCA.cartStateManager.getOrderId(), function (response, status, xhr) {
-                                alert("hit")
+                               // alert("hit")
                                 if (status === 'error') {
                                     $(this).html('<div class="text-error">There has been an error at the server. Please refresh your page and try again. In the event of repeated problems, please use our Help & Feedback button (lower right corner) for immediate assistance.</div>');
 
@@ -1292,7 +1292,8 @@ OCA.wireUpHandlers = function () {
     OCA.displaySetAffiliateModal = function (link) {
 
         // permits Admin to choose which affiliate will be credited with order
-        alert("hit")
+
+        //alert("hit")
         var modalFormOptions = {
             keyboard: true,
             backdrop: 'static',
