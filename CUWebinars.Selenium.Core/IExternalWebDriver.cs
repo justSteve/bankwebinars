3b5114911a08f4c932a6cbec762d56a1b2e90cc2
Steve <steve@juststeve.com>
2026-02-14 18:15:23 -0600

    auto-sync: stage tracked changes

--- CUWebinars.Selenium.Core/IExternalWebDriver.cs
+++
@@ -1,8 +0,0 @@
-namespace CUWebinars.Selenium.Core
-{
-    public interface IExternalWebDriver
-    {
-        int DriverPort { set; }
-        void Initialize();
-    }
-}
--- CUWebinars.Web.Tests/Infrastructure/TestCategories.cs
+++
@@ -1,7 +0,0 @@
-namespace CUWebinars.Web.Tests.Infrastructure
-{
-    public sealed class TestCategories
-    {
-        public const string Orchestrators = "Orchestrators";
-    }
-}
